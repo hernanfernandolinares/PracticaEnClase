d274976f2fdefb7a3a399b408fd467b25150f6f8
hernanfernandolinares <hernanfernandolinares@gmail.com>
2021-08-09 16:01:13 -0300

    1er commit

---
+++ src/main/java/com/practica/practicaEnClase/PracticaEnClaseApplication.java
@@ -0,0 +1,13 @@
+package com.practica.practicaEnClase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaEnClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaEnClaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/practicaEnClase/PracticaEnClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.practicaEnClase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaEnClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
